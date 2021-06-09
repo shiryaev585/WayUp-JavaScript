35cbcdbfa5e2804683d7ef605d0bb55c80f25afb
Alexander Shiryaev <alex_sh_v@mail.ru>
2021-06-09 21:20:44 +0300

    hw update

--- tabs/main.js
+++ tabs/main.js
@@ -1,38 +1,40 @@
 const tabs = document.getElementById('tabs');
 const content = document.querySelectorAll('.content');
 const myTabs = document.getElementById('my-tabs');
+const myContent = document.querySelectorAll('.my-content');
 
-const changeClass = (arr, el) => {
-  for (let i = 0; i < arr.children.length; i++) {
-    arr.children[i].classList.remove('active');
-  }
+const changeClass = (el) => {
+    for (let i = 0; i < tabs.children.length; i++) {
+        tabs.children[i].classList.remove('active');
+      }
   el.classList.add('active');
 }
 
-// обёртка позволяющая избегать дублирование цикла, при появлении нового массива елементов, нуждающихся в смене класса
-const changeClassContainer = (el) => {
-  changeClass(tabs, el)
-  changeClass(myTabs, el)
-};
+const changeMyTabsClass = (el) => {
+    for(let i = 0; i < myTabs.children.length; i++) {
+        myTabs.children[i].classList.remove('active')
+    }
+    el.classList.add('active');
+}
 
-const bindContentWhithTabs = (currentTab) => {
+tabs.addEventListener('click', (e) => {
+  const currentTab = e.target.dataset.btn;
+  changeClass(e.target);
   content.forEach((item) => {
     item.classList.remove('active');
     if (item.dataset.content === currentTab) {
       item.classList.add('active');
     }
   });
-};
-
-tabs.addEventListener('click', (e) => {
-  const currentTab = e.target.dataset.btn;
-  changeClassContainer(e.target);
-  bindContentWhithTabs(currentTab);
 });
 
 myTabs.addEventListener('click', (e) => {
-  const currentTab = e.target.dataset.btn;
-  changeClassContainer(e.target);
-  bindContentWhithTabs(currentTab);
-});
-
+    const currentTab = e.target.dataset.mybtn;
+    changeMyTabsClass(e.target);
+    myContent.forEach((item) => {
+      item.classList.remove('active');
+      if (item.dataset.content === currentTab) {
+        item.classList.add('active');
+      }
+    });
+  });
